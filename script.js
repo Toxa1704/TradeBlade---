7bc66753f507c10e840ae394e2e1ec8db9c78a21
Toxa1704 <Toxa1704@outlook.com>
2025-11-15 12:06:34 +0200

    main

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ container.addEventListener("wheel", (e) => {
   e.preventDefault();
   container.scrollLeft += e.deltaY;
 });
+document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', () => {
+        item.parentElement.classList.toggle('active');
+    });
+});
